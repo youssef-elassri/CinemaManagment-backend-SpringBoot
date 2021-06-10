e29f87201c22427c6956ecfbe8d420bb123c4f62
youssef-elassri <62482692+youssef-elassri@users.noreply.github.com>
2021-06-10 20:54:03 +0100

    Update CinemaInitServiceImp.java

--- src/main/java/cinemaproject/services/CinemaInitServiceImp.java
+++ src/main/java/cinemaproject/services/CinemaInitServiceImp.java
@@ -43,155 +43,123 @@ public class CinemaInitServiceImp implements ICinemaInitService {
 
 	@Autowired
 	private TicketRepository ticketRepository;
-
-	@Override
+@Override
 	public void initVilles() {
-		// TODO Auto-generated method stub
-		Stream.of("Casablanca", "Marrakech", "Rabat", "Tanger").forEach(villeName->{
+		Stream.of("Marrakech", "Agadir", "Rabat", "Casablanca").forEach(nameVille -> {
 			Ville ville = new Ville();
-			ville.setName(villeName);
+			ville.setName(nameVille);
 			villeRepository.save(ville);
 		});
-
 	}
 
 	@Override
 	public void initCinemas() {
-		// TODO Auto-generated method stub
-		villeRepository.findAll().forEach(v->{
-			Stream.of("MegaRama", "IMAX", "FOUNOUN", "CHAHRAZAD", "DAOULIZ")
-			.forEach(nameCinema->{
+		villeRepository.findAll().forEach(v -> {
+			Stream.of("MegaRama", "Imax", "Founoun", "Chahrazad", "Daouliz").forEach(name -> {
 				Cinema cinema = new Cinema();
-				cinema.setName(nameCinema);
-				cinema.setNombreSalles(3+(int)(Math.random()*7));
+				cinema.setName(name);
+				cinema.setNombreSalles((int) (3 + Math.random() * 7));
 				cinema.setVille(v);
 				cinemaRepository.save(cinema);
-
 			});
 		});
-
 	}
 
 	@Override
 	public void initSalles() {
-		cinemaRepository.findAll().forEach(cinema->{
-			for(int i=0; i<cinema.getNombreSalles();i++) {
+		cinemaRepository.findAll().forEach(cinema -> {
+			for (int i = 0; i < cinema.getNombreSalles(); i++) {
 				Salle salle = new Salle();
-				salle.setName("Salle " + i+1 );
+				salle.setName("Salle " + (i + 1));
 				salle.setCinema(cinema);
-				salle.setNombrePlaces(20+(int)(Math.random()*30));
+				salle.setNombrePlace((int) (15 + ((Math.random() * 20))));
 				salleRepository.save(salle);
 			}
 		});
 	}
 
 	@Override
-	public void initSeances() {
-		DateFormat dateFormat=new SimpleDateFormat("HH:mm");
-		Stream.of("12:00","15:00","17:30","19:00","21:30")
-				.forEach(s->{
-					Seance seance=new Seance();
-					try {
-						seance.setHeureDebut(dateFormat.parse(s));
-						seanceRepository.save(seance);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				});
-	}
-
-	@Override
 	public void initPlaces() {
-		salleRepository.findAll()
-				.forEach(salle->{
-					for(int i=0;i<salle.getNombrePlaces();i++) {
-						Place place=new Place();
-						place.setNumero(i+1);
-						place.setSalle(salle);
-						place.setAltitude(Math.random());
-						place.setLatitude(Math.random());
-						place.setLongitude(Math.random());
-						placeRepository.save(place);
-					}
-				});
+		salleRepository.findAll().forEach(salle -> {
+			for (int i = 0; i < salle.getNombrePlace(); i++) {
+				Place place = new Place();
+				place.setNumero((i + 1));
+				place.setSalle(salle);
+				placeRepository.save(place);
+			}
+		});
 	}
 
 	@Override
-	public void initCategories() {
-		Stream.of("Actions","Fiction","Drama","Policier","Comedie")
-				.forEach(cat->{
-					Categorie categorie=new Categorie();
-					categorie.setName(cat);
-					categorieRepository.save(categorie);
-				});
+	public void initSeances() {
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		Stream.of("12:00", "15:00", "17:00", "19:00", "21:00").forEach(s -> {
+			Seance seance = new Seance();
+			try {
+				seance.setHeureDebut(dateFormat.parse(s));
+				seanceRepository.save(seance);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
+	@Override
+	public void initGategories() {
+		Stream.of("Comedy", "Action", "Fiction", "Drama", "Horror").forEach(c -> {
+			Categorie categorie = new Categorie();
+			categorie.setName(c);
+			categorieRepository.save(categorie);
+		});
 	}
 
 	@Override
 	public void initFilms() {
-		double[]durees=new double[] {1,1.5,2,2.5,3};
-		List<Categorie>categories=categorieRepository.findAll();
-		Stream.of("Black Panther","Aladdin","Avengers End Game","Bad Boys 3","Frozen2","Home Alone","The Terminal","HobbsandShaw","Dora")
-				.forEach(titreFilm->{
-					Film film=new Film();
-					film.setTitre(titreFilm);
-					film.setDuree(durees[new Random().nextInt(durees.length)]);
-					film.setPhoto(titreFilm.replaceAll(" ","")+".jpg");
-					film.setCategorie(categories.get(new Random().nextInt(categories.size())));
-					film.setDateSortie(new Date());
-					filmRepository.save(film);
-				});
-
+		double[] durees = new double[] { 1.5, 2, 2.5, 3 };
+		List<Categorie> categories = categorieRepository.findAll();
+		Stream.of("12 years a slave", "tenet", "wrath of a man", "the avengers").forEach(mov -> {
+			Film film = new Film();
+			film.setTitre(mov);
+			film.setDuree(durees[new Random().nextInt(durees.length)]);
+			film.setPhoto(mov.toLowerCase().replaceAll(" ", ""));
+			film.setCategorie(categories.get(new Random().nextInt(categories.size())));
+			filmRepository.save(film);
+		});
 	}
 
 	@Override
 	public void initProjections() {
-		double[]prices=new double[] {30,50,60,70,90,100};
-		List<Film> films=filmRepository.findAll();
-		villeRepository.findAll()
-				.forEach(ville->{
-					ville.getCinemas().forEach(cinema->{
-						cinema.getSalles().forEach(salle->{
-							int index=new Random().nextInt(films.size());
-
-
-							Film film=films.get(index);
-							seanceRepository.findAll()
-									.forEach(seance->{
-										Projection projection=new Projection();
-										projection.setDateProjection(new Date());
-										projection.setFilm(film);
-										projection.setPrix(prices[new Random().nextInt(prices.length)]);
-										projection.setSalle(salle);
-										projection.setSeance(seance);
-										projectionRepository.save(projection);
-									});
-
-						});
-					});
+		double[] prices = new double[] { 30, 50, 70, 90, 100 };
+		List<Film> films = filmRepository.findAll();
+		villeRepository.findAll().forEach(ville -> {
+			ville.getCinemas().forEach(cinema -> {
+				cinema.getSalles().forEach(salle -> {
+					int index= new Random().nextInt(films.size());
+                    Film film = films.get(index);
+                    seanceRepository.findAll().forEach(seance -> {
+                        Projection projection = new Projection();
+                        projection.setFilm(film);
+                        projection.setPrix(prices[new Random().nextInt(prices.length)]);
+                        projection.setSalle(salle);
+                        projection.setSeance(seance);
+                        projectionRepository.save(projection);
+                    });
 				});
-
+			});
+		});
 	}
 
 	@Override
 	public void initTickets() {
-		projectionRepository.findAll()
-				.forEach(p->{
-					p.getSalle().getPlaces().forEach(place->{
-						Ticket ticket=new Ticket();
-						ticket.setPlace(place);
-						ticket.setPrix(p.getPrix());
-						ticket.setProjection(p);
-						ticket.setReserver(false);
-						ticketRepository.save(ticket);
-
-					});
-				});
-
+		projectionRepository.findAll().forEach(projection -> {
+			projection.getSalle().getPlaces().forEach(place -> {
+				Ticket ticket = new Ticket();
+				ticket.setPlace(place);
+				ticket.setPrix(projection.getPrix());
+				ticket.setProjection(projection);
+				ticket.setReserve(false);
+				ticketRepository.save(ticket);
+			});
+		});
 	}
-
-
-
-
 }
